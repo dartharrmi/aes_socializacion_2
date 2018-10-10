6db97e13a048ee2ee98a7fa86f45b37dc4e28aef
miguel-arroyo-movile <miguel.arroyo@movile.com>
2018-10-10 18:29:46 -0500

    Add url property

--- app/src/main/java/com/dartharrmi/aes/pas/borboapp/model/Product.kt
+++ app/src/main/java/com/dartharrmi/aes/pas/borboapp/model/Product.kt
@@ -8,6 +8,7 @@ data class Product(val id: Long = 0,
                    val price: Double = 0.0,
                    val quantity: Long = 0,
                    val idCatalog: Long = 0,
+                   val url: String = "",
                    val sku: Sku? = null) {
 
     constructor(hashMap: HashMap<*, *>) : this(
@@ -15,5 +16,6 @@ data class Product(val id: Long = 0,
             hashMap["name"] as String,
             (hashMap["price"] as String).toDouble(),
             (hashMap["quantity"] as String).toLong(),
-            (hashMap["idCatalog"] as String).toLong())
+            (hashMap["idCatalog"] as String).toLong(),
+            (hashMap["url"] as String))
 }
